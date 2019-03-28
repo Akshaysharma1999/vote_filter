5f403a9ea3427ea9d13c25f8bd994e46fe58267f
akshay sharma <akshay.3.1999@gmail.com>
2019-03-28 19:18:35 +0530

    initial commit

--- routes/root.js
+++ routes/root.js
@@ -1,12 +1,36 @@
 const {Router} = require('express')
 const route = Router()
 
+//function to generate random names
+function random_string(n)
+{
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzUVWXYZabcdefghijklm" 
+    
+    let i=0 , num;
+    
+    while( i == 0 )
+    {
+         num = Math.random()*10 ;
+       
+        if(num >= 1 && num <= 60 )
+        {
+            i=1 ;
+        }
+    }
+
+   return possible.substr(num,n)
+}
+
+
 route.post('/aadhaar_auth',(req,res)=>{
     // res.send('/aadhaar_auth route')
+    let name  = random_string(6)
+    
     res.send(
         {
-            name:"akshay",
+            name:name,
             ph_no:"9582312294",
+            aadhaar_no:"1121232322",
             otp:2294 
         }
     )
