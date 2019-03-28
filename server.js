65989068a90a952d06fb32ae616f90bfc275da95
akshay sharma <akshay.3.1999@gmail.com>
2019-03-28 22:13:39 +0530

    for encoding  post request data

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+
+
+app.use(express.json() )
+app.use(express.urlencoded({extended:true}))
+
 app.use('/',require('./routes/root'))
 
 app.listen('3000',()=>{console.log("http://localhost:3000")})
