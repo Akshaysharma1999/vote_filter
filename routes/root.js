0e005dbb7091a2289d24e83338f7ab723c28a441
bisht-ruchir-007 <32924441+bisht-ruchir-007@users.noreply.github.com>
2019-03-28 22:17:47 +0530

    Update root.js

--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const route = Router()
+var Voter = require('../models/db');
 
 route.post('/aadhaar_auth',(req,res)=>{
     // res.send('/aadhaar_auth route')
@@ -15,6 +16,35 @@ route.post('/aadhaar_auth',(req,res)=>{
 
 route.get('/verified_user',(req,res)=>{
     // res.send('/verified_user route')
+    
+    // seed data 
+    var aadhaar = req.body.aadhaar;
+    var votername = req.body.votername;
+    var age = req.body.age;
+    var phone = req.body.phone;
+    var voterID = req.body.voterID;
+    
+    try{
+        db.collection('Voter').find({aadhaar:aadhaar}).toArray((err, result) => {
+        if (err) {
+            console.log('Search error');
+        } else if (_.isEmpty(result)) {
+            console.log('Voter is not present in the data base')
+            result.save()
+                .then(item => {
+                    res.send("Voter is saved to the database");
+                 })
+                 .catch(err => {
+                    res.status(400).send("Unable to save the voter to the  database");
+              });
+        } else {
+            console.log('Voter is present in the database')
+       };
+     }
+    catch (err) {
+    //console.error("error");
+}                                                           
+});
     
 })
 
